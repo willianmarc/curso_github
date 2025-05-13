1dc26c7a502e9b723188a17e8698830191bced69
devwillian <willianpaixao682@gmail.com>
2025-05-13 07:21:10 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -1,2 +1,9 @@
 //trabalhando no login
 //Finalizando sistema de login
+
+function login (nome ,email){
+    return "Bem vindo"
+}
+
+//Sistema de login completo
+//Retornamos o nome do usuario quando logado
